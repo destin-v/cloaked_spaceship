e89e1abd73b16f5a8ef9d465c0ef5f9072a2a56d
destin-v <47424506+destin-v@users.noreply.github.com>
2024-09-12 20:15:23 -0400

    Remove comment header from nox

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,3 @@
-"""
-Nox creates a new virtual environment for each individual test.  Thus, it is important for to install all the packages needed for testing.  When using Nox, it will by default grab the current python version available in your environment and run testing with it.
-
-Useful commands:
-
-```console
-nox --list                 # Lists out all the available sessions
-nox -r -s pytest           # Run pytests
-nox -r -s pytest_cov       # Run pytests with coverage report
-nox -r -s coverage         # Run coverage (stand-alone)
-nox -r -s scalene          # Profile the code
-
-nox                        # Run all sessions
-```
-
-"""
-
 from dataclasses import dataclass
 
 import nox
